381c57ebeaf0341944c28a0c7e48177a10cead68
Osamu Monoe <osamum@microsoft.com>
2023-12-11 14:36:08 +0900

    fix API url

--- js/site.js
+++ js/site.js
@@ -26,7 +26,6 @@ function showListView() {
 }
 
 async function rederMovieList(arg, itemListCtrl) {
-    //const END_POINT = 'api/movie' + arg;
     const response = await fetch(MOVIEAPP_API);
     const responseData = await response.json();
     if (Array.isArray(responseData)) {
